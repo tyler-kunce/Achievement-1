e853b206a9798ac00d4d6def0f0e160705285c60
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2024-10-02 10:08:41 -0500

    Feedback implementation

--- Exercise-1.2/recipes.py
+++ Exercise-1.2/recipes.py
@@ -1,9 +1,9 @@
-recipe_1 = { 'Name': 'Tea', 'Cooking Time': '5 minutes', 'Ingredients': ['Tea leaves', 'Sugar', 'Water']}
+recipe_1 = { 'name': 'Tea', 'cooking_time': '5 minutes', 'ingredients': ['Tea leaves', 'Sugar', 'Water']}
 
-recipe_2 = { 'Name': 'Chili', 'Cooking Time': '8.5 hours', 'Ingredients': ['Ground turkey', 'Black beans', 'Chopped onion', 'Rotel tomato', 'Seasoning']}
+recipe_2 = { 'name': 'Chili', 'cooking_time': '8.5 hours', 'ingredients': ['Ground turkey', 'Black beans', 'Chopped onion', 'Rotel tomato', 'Seasoning']}
 
-recipe_3 = { 'Name': 'Buffalo wings', 'Cooking Time': '1 hour', 'Ingredients': ['Chicken wings', 'Frank\'s Red Hot Sauce', 'Butter', 'White vinegar', 'Worcestershire sauce', 'Garlic powder', 'Cayenne pepper']}
+recipe_3 = { 'name': 'Buffalo wings', 'cooking_time': '1 hour', 'ingredients': ['Chicken wings', 'Frank\'s Red Hot Sauce', 'Butter', 'White vinegar', 'Worcestershire sauce', 'Garlic powder', 'Cayenne pepper']}
 
-recipe_4 = { 'Name': 'Coffee', 'Cooking Time': '5 minutes', 'Ingredients': ['Coffee grounds', 'Water', 'Milk (to taste)', 'Sugar (to taste)']}
+recipe_4 = { 'name': 'Coffee', 'cooking_time': '5 minutes', 'ingredients': ['Coffee grounds', 'Water', 'Milk (to taste)', 'Sugar (to taste)']}
 
-recipe_5 = { 'Name': 'Omelet', 'Cooking Time': '10 minutes', 'Ingredients': ['Eggs', 'Spinach', 'Chopped onion', 'Cheese']}
+recipe_5 = { 'name': 'Omelet', 'cooking_time': '10 minutes', 'ingredients': ['Eggs', 'Spinach', 'Chopped onion', 'Cheese']}
